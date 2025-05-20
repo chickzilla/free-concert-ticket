fd1918d6d04eaa48aea19ab123b7c209de444330
Dulyawat Khamkaew <6430127621@student.chula.ac.th>
2025-05-20 14:39:29 +0700

    web: integrate all total of seat

--- web/src/components/home/stat.tsx
+++ web/src/components/home/stat.tsx
@@ -1,17 +1,30 @@
 "use client";
 import { useEffect, useState } from "react";
 import StatItem from "./stat-item";
+import allTotalOfSeat from "@/service/concert/allTotalOfSeat";
 
 export default function Stat() {
-  const [totalOfSeats, setTotalOfSeats] = useState(500);
+  const [totalOfSeats, setTotalOfSeats] = useState(0);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
-      // TODO api
+      try {
+        const totalOfSeats = await allTotalOfSeat();
+        setTotalOfSeats(totalOfSeats?.total_of_seat);
+        setLoading(false);
+      } catch (error) {
+        if (error instanceof Error) {
+          alert("Error fetching data: " + error.message);
+        }
+      }
     };
     fetchData();
   }, []);
 
+  if (loading) {
+    return;
+  }
   return (
     <div className="w-full px-4">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 ">
@@ -20,8 +33,8 @@ export default function Stat() {
           value={totalOfSeats}
           bg="bg-[#0070A4]"
         />
-        <StatItem title="Reserve" value={500} bg="bg-[#00A58B]" />
-        <StatItem title="Cancel" value={12} bg="bg-[#E84E4E]" />
+        <StatItem title="Reserve" value={0} bg="bg-[#00A58B]" />
+        <StatItem title="Cancel" value={0} bg="bg-[#E84E4E]" />
       </div>
     </div>
   );
---
+++ web/src/service/concert/allTotalOfSeat.ts
@@ -0,0 +1,17 @@
+import { allTotalOfSeatResponse } from "@/interface";
+
+export default async function allTotalOfSeat(): Promise<allTotalOfSeatResponse> {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/concert/totalOfSeat`,
+    {
+      method: "GET",
+    }
+  );
+
+  if (!res.ok) {
+    const error = await res.json();
+    throw new Error(error.message);
+  }
+
+  return await res.json();
+}
