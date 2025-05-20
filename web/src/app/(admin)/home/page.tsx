be9dc110a45577d7c1a9a26280a1cb888ff32357
Dulyawat Khamkaew <6430127621@student.chula.ac.th>
2025-05-20 16:21:40 +0700

    web: change background color

--- web/src/app/(admin)/home/page.tsx
+++ web/src/app/(admin)/home/page.tsx
@@ -3,7 +3,7 @@ import Stat from "@/components/home/stat";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center min-h-screen space-y-4 pt-10">
+    <div className="flex flex-col items-center min-h-screen space-y-4 pt-10 bg-[#fbfbfb]">
       <Stat />
       <ConcertTab />
     </div>
